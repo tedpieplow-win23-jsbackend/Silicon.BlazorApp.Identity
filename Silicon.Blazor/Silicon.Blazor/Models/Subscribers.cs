ecbb491000932f68289cde008ba93c744c2791d0
TPieplow <ted.pieplow@gmail.com>
2024-05-10 14:32:11 +0200

    fungera

--- Silicon.Blazor/Silicon.Blazor/Models/Subscribers.cs
+++ Silicon.Blazor/Silicon.Blazor/Models/Subscribers.cs
@@ -5,6 +5,4 @@ public class Subscribers
     public string Email { get; set; } = null!;
     public bool IsSubscribed { get; set; }
 
-    // URI till Toggle sub
-    // https://subscriptionprovider-silicon.azurewebsites.net/api/ToggleSubscriptionFunction?code=eBz-AH-lVbIir3LGga3ky_zdNHUoYDUfcKnNGXc-ejH1AzFuw1UUhg==
 }

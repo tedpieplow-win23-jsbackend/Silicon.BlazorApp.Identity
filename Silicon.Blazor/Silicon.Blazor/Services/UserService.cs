017adb0da6cf6051af9a213aa5bc0c7637b0a84b
TPieplow <ted.pieplow@gmail.com>
2024-05-10 15:49:56 +0200

    stash

--- Silicon.Blazor/Silicon.Blazor/Services/UserService.cs
+++ Silicon.Blazor/Silicon.Blazor/Services/UserService.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
 using Silicon.Blazor.Data;
 using Silicon.Blazor.Models;
 using System.Security.Claims;
 
 namespace Silicon.Blazor.Services;
 
-public class UserService(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+public class UserService(ApplicationDbContext context, UserManager<ApplicationUser> userManager, HttpClient httpClient, IConfiguration configuration)
 {
     private readonly ApplicationDbContext _context = context;
     private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly IConfiguration _configuration = configuration;
 
     public async Task<ResponseResult> ManageSubscription(bool isSubscribed, ClaimsPrincipal userClaims)
     {
@@ -19,11 +22,21 @@ public class UserService(ApplicationDbContext context, UserManager<ApplicationUs
             {
                 loggedInUser.IsSubscribed = isSubscribed;
                 await _context.SaveChangesAsync();
-                return ResponseFactory.Ok();
+
+                var email = loggedInUser?.Email;
+                var apiUrl = _configuration.GetValue<string>("ConnectionStrings:ToggleSubscription");
+                var requestData = new { email, isSubscribed };
+                var jsonContent = JsonConvert.SerializeObject(requestData);
+                var httpResponse = await _httpClient.PostAsJsonAsync(apiUrl, jsonContent);
+
+                if (httpResponse.IsSuccessStatusCode)
+                    return ResponseFactory.Ok();
+                else
+                    return ResponseFactory.Error("API-request failed.");
             }
             else
             {
-                return ResponseFactory.NotFound("User doesn't exists.");
+                return ResponseFactory.NotFound("User email is null.");
             }
         }
         catch (Exception ex)

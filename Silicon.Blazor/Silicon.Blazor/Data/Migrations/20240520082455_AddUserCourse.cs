535a920db4fdea790ba1608f0e375390447ffceb
TPieplow <ted.pieplow@gmail.com>
2024-05-20 11:33:18 +0200

    stage: Now possible to save courses by clicking the bookmark icon, which also changes color when saved. Need to implement unregister on courses. Added new entity to db, UserCourses

---
+++ Silicon.Blazor/Silicon.Blazor/Data/Migrations/20240520082455_AddUserCourse.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Silicon.Blazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserCourses",
+                columns: table => new
+                {
+                    UserCourseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CourseId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCourses", x => x.UserCourseId);
+                    table.ForeignKey(
+                        name: "FK_UserCourses_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCourses_ApplicationUserId",
+                table: "UserCourses",
+                column: "ApplicationUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserCourses");
+        }
+    }
+}
